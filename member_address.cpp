9ac2ed4bfc999b17b9aa68376d555aa076e26acb
Krzysztof Siwek <krzysztof.siwek1997@gmail.com>
2022-01-28 20:47:18 +0100

    member func add func

--- member_address.cpp
+++ member_address.cpp
@@ -13,17 +13,16 @@ template<typename T, typename U>
 using MemberPtr = U T::*;
 
 struct B{
-
     template<typename T, typename... U>
-    void test(int a, MemberPtr<T, U>... u){
-       T t{};
-       ((t .*  u=  a++),...);
+    void test(std::function<A(int)> func, MemberPtr<T, U>... u){
+       T t{};int i = 5;
+       ((t.*u =  func(i).*u),...);
        ((std::cout <<  t .* u), ...) << std::endl ;
     }
 };
 
 int main(){
-   int A::* p = &A::a;
-   void (A:: *fp)() = &A::e;
-   B().test<A, int>(2, &A::a, &A::b, &A::c, &A::d);
+ //  int A::* p = &A::a;
+//   void (A:: *fp)() = &A::e;
+   B().test<A, int>([](int a)->A{return A{a++,a++,a++,a++};}, &A::a, &A::b, &A::c, &A::d);
 }
