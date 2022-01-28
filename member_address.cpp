ca61afcd2368595824e7e9fe7a0ad05085f1ed34
Krzysztof Siwek <krzysztof.siwek1997@gmail.com>
2022-01-28 21:13:50 +0100

    member pointers finished c++17

--- member_address.cpp
+++ member_address.cpp
@@ -1,7 +1,24 @@
 #include <functional>
 #include <iostream>
+#include <tuple>
 
 struct A{
+    int a;
+    int b;
+    int c;
+    int d;
+    void e(){};
+};
+
+struct B{
+    double padding;
+    int a;
+    int b;
+    void e(){};
+};
+
+struct C{
+    char x[50];
     int a;
     int b;
     int c;
@@ -12,17 +29,26 @@ struct A{
 template<typename T, typename U>
 using MemberPtr = U T::*;
 
-struct B{
+template<typename... Ts>
+struct Test{
     template<typename T, typename... U>
-    void test(std::function<A(int)> func, MemberPtr<T, U>... u){
+    void test(std::function<T(int)> func, MemberPtr<T, U>... u){
        T t{};int i = 5;
+       std::cout << "NEW MEMBER:\n";
        ((t.*u =  func(i).*u),...);
-       ((std::cout <<  t .* u), ...) << std::endl ;
+       ((std::cout <<  t.* u), ...) << std::endl << "Now assigned: " << std::endl;
+       std::get<T>(Types) = t;
+       ((std::cout << std::get<T>(Types).* u), ...) << std::endl ;
     }
+    
+    std::tuple<Ts...> Types;
 };
 
 int main(){
  //  int A::* p = &A::a;
 //   void (A:: *fp)() = &A::e;
-   B().test<A, int>([](int a)->A{return A{a++,a++,a++,a++};}, &A::a, &A::b, &A::c, &A::d);
+   Test<A,B,C> b;
+   b.test<A>([](int a)->A{return A{a++,a++,a++,a++};}, &A::a, &A::b, &A::c, &A::d);
+   b.test<B>([](int a)->B{return B{0.0, a++,a++};}, &B::a, &B::b, &B::padding);
+   b.test<C>([](int a)->C{return C{{}, a--,a--,a--,a--};}, &C::a, &C::b, &C::c, &C::d);
 }
